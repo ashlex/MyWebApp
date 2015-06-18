8e55dca3969aaec1a3e4704fad99bd75dd158f8d
ashlex <angeldust.ru@gmail.com>
2015-06-19 01:38:03 +0300

    added json_simple lib

--- src/main/MyServlet.java
+++ src/main/MyServlet.java
@@ -15,11 +15,7 @@ public class MyServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<h1>Hello Servlet Get POST</h1>");
-		out.println("</body>");
-		out.println("</html>");
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
